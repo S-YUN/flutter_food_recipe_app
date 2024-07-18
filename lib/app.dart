16e4a1969401c9ff4db1fe99b7b5c0ac96e67ee6
S-YUN <https://github.com/S-YUN>
2024-07-18 10:49:26 +0900

    feat: go_router 적용

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_recipe/data/data_source/recipe_api_fake.dart';
-import 'package:food_recipe/data/repository/recipe_repository_impl.dart';
-import 'package:food_recipe/presentation/main_screen.dart';
+import 'package:food_recipe/config/routes.dart';
 import 'package:food_recipe/ui/color_styles.dart';
 import 'package:food_recipe/ui/size_config.dart';
 
@@ -10,22 +8,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final recipeApi = RecipeApiFake();
-    final recipeRepository = RecipeRepositoryImpl(recipeApi: recipeApi);
-
     SizeConfig().init(context);
 
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Food Recipe',
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: ColorStyles.primary100,
-       
       ),
       debugShowCheckedModeBanner: false,
-      home: MainScreen(
-        recipeRepository: recipeRepository,
-      ),
+      routerConfig: Routes.router,
     );
   }
 }
---
+++ lib/config/routes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_recipe/data/data_source/recipe_api_fake.dart';
+import 'package:food_recipe/data/model/recipe.dart';
+import 'package:food_recipe/data/repository/recipe_repository_impl.dart';
+import 'package:food_recipe/presentation/main_screen.dart';
+import 'package:food_recipe/presentation/recipe_detail/recipe_detail_screen.dart';
+import 'package:go_router/go_router.dart';
+
+class Routes {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          final recipeApi = RecipeApiFake();
+          final recipeRepository = RecipeRepositoryImpl(recipeApi: recipeApi);
+          return MainScreen(recipeRepository: recipeRepository);
+        },
+      ),
+      GoRoute(
+        path: '/recipeDetail',
+        builder: (BuildContext context, GoRouterState state) {
+          final recipe = state.extra as Recipe;
+          return RecipeDetailScreen(
+            recipe: recipe,
+          );
+        },
+      ),
+    ],
+  );
+}
