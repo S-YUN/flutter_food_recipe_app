59ff9a0395cbe8fc03e3d8ede03c9c04f2bf3525
S-YUN <https://github.com/S-YUN>
2024-07-25 16:10:45 +0900

    viewmodel에 ui state holder 적용

--- lib/presentation/recipe_detail/recipe_detail_screen_view_model.dart
+++ lib/presentation/recipe_detail/recipe_detail_screen_view_model.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:food_recipe/data/model/chef.dart';
-import 'package:food_recipe/data/model/ingredient.dart';
-import 'package:food_recipe/data/model/procedure.dart';
 import 'package:food_recipe/data/repository/chef/chef_repository.dart';
 import 'package:food_recipe/data/repository/ingredient/ingredient_repository.dart';
 import 'package:food_recipe/data/repository/procedure/procedure_repository.dart';
+import 'package:food_recipe/presentation/recipe_detail/recipe_detail_screen_ui_state.dart';
 
 class RecipeDetailScreenViewModel with ChangeNotifier {
   final IngredientRepository _ingredientRepository;
@@ -19,42 +17,25 @@ class RecipeDetailScreenViewModel with ChangeNotifier {
         _procedureRepository = procedureRepository,
         _chefRepository = chefRepository;
 
-  Chef _chefInfo = const Chef(
-      name: '',
-      location: '',
-      imageUrl: '',
-      recipeCount: -1,
-      followers: -1,
-      following: -1,
-      introduce: '');
-  Chef get chefInfo => _chefInfo;
-
-  List<Ingredient> _ingredients = [];
-  List<Ingredient> get ingredients => _ingredients;
-
-  List<Procedure> _procedures = [];
-  List<Procedure> get procedures => _procedures;
-
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
-
-  RecipeDetailInnerTabType _tabType = RecipeDetailInnerTabType.ingredient;
-  RecipeDetailInnerTabType get tabType => _tabType;
+  RecipeDetailScreenUiState _state = const RecipeDetailScreenUiState();
+  RecipeDetailScreenUiState get state => _state;
 
   void fetchAllData({required int recipeId, required String chefName}) async {
-    _isLoading = true;
+    _state = state.copyWith(isLoading: true);
     notifyListeners();
-    _chefInfo = await _chefRepository.getChefInfo(name: chefName);
-    _ingredients =
-        await _ingredientRepository.getRecipeIngredients(recipeId: recipeId);
-    _procedures =
-        await _procedureRepository.getRecipeProcedure(recipeId: recipeId);
-    _isLoading = false;
+    _state = state.copyWith(
+      chefInfo: await _chefRepository.getChefInfo(name: chefName),
+      ingredients:
+          await _ingredientRepository.getRecipeIngredients(recipeId: recipeId),
+      procedures:
+          await _procedureRepository.getRecipeProcedure(recipeId: recipeId),
+      isLoading: false,
+    );
     notifyListeners();
   }
 
   void changeTabType(RecipeDetailInnerTabType value) {
-    _tabType = value;
+    _state = state.copyWith(tabType: value);
     notifyListeners();
   }
 }
