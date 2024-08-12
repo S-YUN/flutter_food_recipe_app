c98f9292486a79ec6e9e35d5e8b019c04b0926af
S-YUN <https://github.com/S-YUN>
2024-08-12 19:19:15 +0900

    integration_test setting

---
+++ integration_test/app_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('어쩌구', (tester) async {});
+  });
+}
